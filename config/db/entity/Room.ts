e7b58c8a7018c913ac2b73b0e33b6283c062c3fb
nyh <nsi3358@naver.com>
2024-05-24 17:37:26 +0900

    FEAT : 웹소켓 이벤트 기능 추가
    - 채팅 구현
    - 방입장 구현
    - 연결 끊김 구현
    - #8

--- config/db/entity/Room.ts
+++ config/db/entity/Room.ts
@@ -5,6 +5,9 @@ export class Room {
   @ObjectIdColumn()
   _id: ObjectId;
 
+  @Column({ unique: true })
+  managerId: string; //유저의 id값을 참조
+
   @Column()
   roomTitle: string;
 
@@ -18,7 +21,7 @@ export class Room {
   playListUrl: string;
 
   @Column('simple-array')
-  roomMember: string[];
+  roomMember: { userId: string; nickName: string }[];
 
   @Column('text', { array: true, default: [] })
   chats: { nickName: string; chat: string }[];
--- src/websocket/socket-manager .ts
+++ src/websocket/socket-manager .ts
@@ -1,5 +1,8 @@
+import { ObjectId } from 'mongodb';
 import { Server, ServerOptions } from 'socket.io';
 import { Server as httpServer } from 'http';
+import { AppDataSource } from '../../config/db/data-source.js';
+import { Room } from '../../config/db/entity/Room.js';
 
 class SocketManager {
   private io;
@@ -11,19 +14,49 @@ class SocketManager {
 
   private setupEventHandlers() {
     this.io.on('connection', socket => {
-      socket.on('chat', (chatData, roomId) => {
-        this.io.to(roomId).emit('chat', '채팅 데이터'); //해당 방에 댓글 데이터 전송
+      socket.on('chat', async (chatData: { nickName: string; chat: string }, roomId: string) => {
+        const roomRepository = AppDataSource.getRepository(Room);
+        const room = await roomRepository.findOne({ where: { _id: new ObjectId(roomId) } });
+        if (room) {
+          room.chats.push(chatData);
+          await roomRepository.save(room);
+          this.io.to(room._id.toString()).emit('chat', chatData); //해당 방에 댓글 데이터 전송
+        }
       });
 
-      socket.on('joinRoom', (roomId, user) => {
-        socket.join(roomId); //방 입장
-        this.io.to(roomId).emit('updateUser', '유저들 데이터'); //해당 방에 참여한 유저들 전송
+      socket.on('joinRoom', async (roomId: string, user: { nickName: string; userId: string }) => {
+        const roomRepository = AppDataSource.getRepository(Room);
+        const room = await roomRepository.findOne({ where: { _id: new ObjectId(roomId) } });
+
+        if (room) {
+          room.roomMember.push(user);
+          await roomRepository.save(room);
+          const stringRoomId = room._id.toString();
+          socket.join(stringRoomId); //방 입장
+          this.io.to(stringRoomId).emit('updateUser', room.roomMember); //해당 방에 참여한 유저들 전송
+        }
       });
 
-      socket.on('disconnect', () => {
-        const userId = socket.handshake.query.userId;
+      socket.on('disconnect', async () => {
+        const userId = socket.handshake.query.userId as string;
+        const room = await AppDataSource.getMongoRepository(Room).findOne({
+          where: { roomMember: { $eleMatch: { userId: userId } } },
+        });
 
-        //이후 추가 로직...
+        if (room) {
+          const stringRoomId = room._id.toString();
+          //만약 연결 끊긴 유저가 방장이면
+          if (room.managerId === userId) {
+            const message = '방장의 연결이 끊겨 방을 해체합니다';
+            this.io.to(stringRoomId).emit('boom', message);
+            return AppDataSource.mongoManager.delete(Room, room._id);
+          }
+
+          //방장이 아니면 접속중인 맴버 목록에서 제거
+          const refreshMember = room.roomMember.filter(member => member.userId !== userId);
+          await AppDataSource.mongoManager.updateOne(Room, { _id: room._id }, { $set: { roomMember: refreshMember } });
+          this.io.to(stringRoomId).emit('updateUser', refreshMember);
+        }
       });
     });
   }
