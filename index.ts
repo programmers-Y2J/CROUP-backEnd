d5322c5d5183d521c6842071271c089fed67ab17
jeehdev <jeehdev@gmail.com>
2024-06-07 12:48:34 +0900

    jwt token 인증 코드 수정

--- index.ts
+++ index.ts
@@ -5,19 +5,21 @@ import { AppDataSource } from './config/db/data-source.js';
 import cookieParser from 'cookie-parser';
 import SocketManager from './src/websocket/socket-manager .js';
 import userRouter from './src/user/user-router.js';
-import roomRouter from './src/room/room-router.js';
+import roomRouter from './src/room/room-router.js'
 import qnaRouter from './src/qna/qna-router.js';
 
 const app = express();
 const server = http.createServer(app);
-new SocketManager(server, { path: '/socket', cors: { origin: '*' } });
+new SocketManager(server, { path: '/rooms', cors: { origin: '*' } });
 
 app.use(express.json());
+
 app.use(cors());
 app.use(cookieParser());
 
 app.use('/auth', userRouter);
 app.use('/rooms', roomRouter);
+
 
 const port = 5000;
 server.listen(port, async () => {
