7abcba61161a177f3b727bd3621ffc07d3f911b0
nyh <nsi3358@naver.com>
2024-07-07 16:23:04 +0900

    FEAT : 닉네임, 비밀번호 변경 기능 추가

--- src/user/user-router.ts
+++ src/user/user-router.ts
@@ -1,19 +1,19 @@
-import express from 'express';
-import userController from './user-controller.js';
-import userErrorHandler from './user-error-handler.js';
-import authMiddleware from '../token/auth-middleware.js';
+import express from "express";
+import userController from "./user-controller.js";
+import userErrorHandler from "./user-error-handler.js";
+import authMiddleware from "../token/auth-middleware.js";
 
 const userRouter = express.Router();
 
-userRouter.post('/join', userController.join);
+userRouter.post("/join", userController.join);
 
-userRouter.post('/login', userController.login);
+userRouter.post("/login", userController.login);
 
-userRouter.post('/logout', userController.logout);
+userRouter.post("/logout", userController.logout);
 
-userRouter.put('/change/name', authMiddleware, userController.replaceName);
+userRouter.put("/name", authMiddleware, userController.replaceName);
 
-userRouter.put('/change/password', authMiddleware, userController.replacePwd);
+userRouter.put("/password", authMiddleware, userController.replacePwd);
 
 userRouter.use(userErrorHandler.catch);
 
